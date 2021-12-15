99f88577af83e37d3527e1a7cdc7c412c0f5f869
Dmitry Podelnik <45734486+DmitryPodelnik@users.noreply.github.com>
2021-12-15 17:20:10 +0200

    1. Smoke Test

---
+++ token_sale/contracts/DappToken.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.16;
+
+contract DappToken {
+    // Constructor
+    // Set the total number of tokens
+    // Read the total number of tokens
+    uint256 public totalSupply;
+    constructor() public {
+        totalSupply = 1000000;
+    }
+}
