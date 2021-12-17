55d58d607965ce04d877a97fe3fedae81a37d11e
Dmitry Podelnik <45734486+DmitryPodelnik@users.noreply.github.com>
2021-12-17 17:17:23 +0200

    Front-End

--- token_sale/migrations/2_deploy_contracts.js
+++ token_sale/migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 const DappToken = artifacts.require("DappToken");
+const DappTokenSale = artifacts.require("DappTokenSale");
 
 module.exports = function(deployer) {
-  deployer.deploy(DappToken, 1000000);
+  deployer.deploy(DappToken, 1000000).then(() => {
+    // Token price is 0.0-1 Ether
+    let tokenPrice = 1000000000000000; 
+    return deployer.deploy(DappTokenSale, DappToken.address, tokenPrice);
+  });
 };
